354742834b1e0ab0f90946480e2f14eeb47b5c66
Airdrop_scripts <103510868+Hyperosislove@users.noreply.github.com>
2024-12-01 10:59:00 +0500

    Create message_templates.py

---
+++ message_templates.py
@@ -0,0 +1,15 @@
+# message_templates.py
+message_templates = {
+    'en': {
+        'start': "Welcome to the bot!",
+        'language_selection': "Please choose your language:",
+        'help': "This is a help message.",
+        'about': "About the bot.",
+        'newtopic': "Starting a new topic.",
+        'image_prompt': "Please provide a description for the image.",
+        'image_error': "An error occurred while generating the image.",
+        'processing': "Processing your request...",
+        'language_confirmation': "Language has been set."
+    },
+    # Add other languages (ru, ua) here
+}
